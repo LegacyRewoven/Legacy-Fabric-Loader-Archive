5d763a1a4708e222a1adb5b6c0e62fa958cd8fad
SD <harrypotter.starwars.shrish@gmail.com>
2021-02-09 10:11:55 +0530

    Fix mixin testmod

--- src/test/java/net/fabricmc/test/mixin/MixinGuiMain.java
+++ src/test/java/net/fabricmc/test/mixin/MixinGuiMain.java
@@ -19,8 +19,6 @@ package net.fabricmc.test.mixin;
 
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -28,14 +26,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = TitleScreen.class, remap = false)
 public abstract class MixinGuiMain extends Screen {
-
-	protected MixinGuiMain(Text textComponent_1) {
-		super(textComponent_1);
-	}
-
 	@Inject(method = "render", at = @At("RETURN"))
-	public void render(MatrixStack matrixStack, int mouseX, int mouseY, float delta, CallbackInfo info) {
-		this.textRenderer.draw(matrixStack, "Fabric Test Mod", 2, this.height - 30, -1);
+	public void render(int i, int j, float f, CallbackInfo ci) {
+		this.textRenderer.draw("Fabric Test Mod", 2, this.height - 30, -1);
 	}
-
 }
